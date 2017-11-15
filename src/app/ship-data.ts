dc382464125db3e71232c197b3123a0fd075642f
Kurotsuba <kanneco98@gmail.com>
2017-11-15 17:09:17 +0800

    add data

--- src/app/ship-data.ts
+++ src/app/ship-data.ts
@@ -2,4 +2,20 @@ import { Ship } from './ship';
 
 export const SHIPS: Ship[] = [
     {c:"BB", name:"YAMATO", caliber: 460, tonnage:69100, length: 263},
+    {c:"BB", name:"KONGO", caliber: 356, tonnage:31720, length: 219.4},
+    {c:"BB", name:"FUSO", caliber: 356, tonnage:34700, length: 212.75},
+    {c:"CV", name:"SHINANO", caliber: 0, tonnage:71890, length: 266},
+    {c:"CV", name:"SHOGAKU", caliber: 0, tonnage:32105, length: 257.5},
+    {c:"CV", name:"HIRYU", caliber: 0, tonnage:17300, length: 216.9},
+    {c:"CV", name:"AKAGI", caliber: 0, tonnage:36500, length: 260.67},
+    {c:"CA", name:"AOBA", caliber: 203, tonnage:10225, length: 185.17},
+    {c:"CA", name:"OYODO", caliber: 155, tonnage:11443, length: 192},
+    {c:"CA", name:"MAYA", caliber: 203, tonnage:15159, length: 203.76},
+    {c:"CL", name:"JINTSU", caliber: 140, tonnage:5195, length: 162.46},
+    {c:"CL", name:"ABUKUMA", caliber: 140, tonnage:5570, length: 162.15},
+    {c:"CL", name:"ISUZU", caliber: 140, tonnage:5570, length: 162.15},
+    {c:"DD", name:"SHIMAKAZE", caliber: 127, tonnage:2500, length: 129.5},
+    {c:"DD", name:"SHIGURE", caliber: 127, tonnage:1712, length: 107.5},
+    {c:"DD", name:"YUKIKAZE", caliber: 127, tonnage:2490, length: 118.49},
+    {c:"DD", name:"FUBUKI", caliber: 127, tonnage:1900, length: 118.5},
 ]
